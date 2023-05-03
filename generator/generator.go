3fba9ea27316c5ea3a6b95ea4c9010ddf7734ec7
Koki Fukuda <ko.fu.dev@gmail.com>
2023-05-03 11:49:57 +0900

    Fix

--- generator/generator.go
+++ generator/generator.go
@@ -9,6 +9,15 @@ import (
 	"github.com/youpy/go-wav"
 )
 
+func getSampleValue(ratio float64) float64 {
+	return math.Sin(ratio * math.Pi * 2.0)
+}
+
+func calculateRatio(sampleNo, samplePerSec, freq int) float64 {
+	samplePerCycle := samplePerSec / freq
+	return float64(sampleNo%samplePerCycle) / float64(samplePerCycle)
+}
+
 func Generate(data tokenizer.RawData) error {
 	out, err := os.Create("/tmp/hoge.wav")
 	if err != nil {
@@ -19,12 +28,13 @@ func Generate(data tokenizer.RawData) error {
 	bufWriter := bufio.NewWriter(out)
 	defer bufWriter.Flush()
 
-	encoder := wav.NewWriter(out, 44100, 2, 44100, 16)
+	encoder := wav.NewWriter(out, 88200, 2, 44100, 16)
 
-	for i := 0; i < 44100; i++ {
+	for i := 0; i < 88200; i++ {
 		sample := wav.Sample{}
 
-		sampleVal := int(math.Sin((44100%440)/440.0*2*math.Pi) * 32768)
+		sampleVal := (int(getSampleValue(calculateRatio(i, 44100, 261))*32767) + int(getSampleValue(calculateRatio(i, 44100, 130))*32767) + int(getSampleValue(calculateRatio(i, 44100, 522))*32767)) / 3
+		sampleVal = int(float64(sampleVal) * (getSampleValue(calculateRatio(i, 44100, 6))*0.3 + 1.7) / 2.0)
 
 		sample.Values[0] = sampleVal
 		sample.Values[1] = sampleVal
